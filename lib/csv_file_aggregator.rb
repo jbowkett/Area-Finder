9993898c17a3519a3eee12d09bc9bcceb1039194
jbowkett <james_bowkett@lyagushka.co.uk>
2013-02-11 22:51:33 +0000

    creates aggregated output file

--- lib/csv_file_aggregator.rb
+++ lib/csv_file_aggregator.rb
@@ -8,12 +8,27 @@ class CSVFileAggregator
   end
 
   def aggregate
+    output = File.new("output.csv", "w")
+
+    output.write("Input File,Station,Average House Price,Destination,Duration,Frequency,Changes,Primary Schools Link,Secondary Schools Link\n")
+
     Dir.foreach(@input_dir) do |file_name|
       next if file_name == '.' || file_name == '..'
       puts "Inspecting :[#{file_name}]..."
       areas = InputCsvParser.new.parse(File.join(@input_dir, file_name))
+      areas.each do |key, value|
+        escaped_name = key.gsub(/ /, "%20")
+
+        primary_url = "http://home.rm.com/schoolfinder/ShowSchools.aspx?l=#{escaped_name}&t=pri"
+        secondary_url = "http://home.rm.com/schoolfinder/ShowSchools.aspx?l=#{escaped_name}&t=sec"
+        area_details = "#{file_name},#{value.name},#{value.average_house_price}"
+
+        value.journeys.each{|journey| output.write "#{area_details},#{journey.destination_station},#{journey.duration},#{journey.frequency},#{journey.changes},#{primary_url},#{secondary_url}\n" }
+      end
+
       puts "Done. #{areas.size}"
     end
+    output.close
   end
 end
 
