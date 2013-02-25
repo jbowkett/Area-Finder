ed21ca3edc6c4ba505e190dbd4f1c4323d56f46e
jbowkett <james_bowkett@lyagushka.co.uk>
2013-02-25 23:01:23 +0000

    moved main entry point to main script

--- lib/Main.rb
+++ lib/Main.rb
@@ -1,8 +1,6 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..")
-require 'bundler'
-require 'scrapi'
-require 'lib/area'
-require 'lib/input_csv_parser'
+
+require_relative 'csv_file_aggregator_and_loader'
 
 ActiveRecord::Base.establish_connection(
     :adapter  => "mysql",
@@ -12,41 +10,10 @@ ActiveRecord::Base.establish_connection(
     :database => "area_finder"
 )
 
+input_dir = ARGV[0]
 
+aggregator = CSVFileAggregatorAndLoader.new(input_dir)
 
-parser = InputCsvParser.new
+aggregator.aggregate
 
-areas = parser.parse 'input-files/north-to-anywhere-10-50-minutes.csv'
-
-
-areas.each_value { |area|
-  puts area
-
-  Area.transaction do
-
-    area.journeys.each {
-      | journey |
-      journey.save
-    }
-    area.save
-
-  end
-
-
-}
-
-
-
-#puts "hello"
-#scraper = Scraper.define do
-#puts "world"
-#  process "title", :page_name => :text
-#  result :page_name
-#end
-#
-#puts "one"
-#uri = URI.parse("http://www.bbc.co.uk/news/")
-##uri = URI.parse("http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=LOST+third+season&Find.x=17&Find.y=1&Find=Find")
-#puts "two"
-#puts scraper.scrape(uri)
 
--- lib/csv_file_aggregator_and_loader.rb
+++ lib/csv_file_aggregator_and_loader.rb
@@ -4,13 +4,12 @@ class CSVFileAggregatorAndLoader
 
   def initialize(input_dir)
     @input_dir = input_dir
-    ::InputCsvParser.new
   end
 
   def aggregate
-    output = File.new("commute-from-aggregated.csv", "w")
+    #output = File.new("commute-from-aggregated.csv", "w")
 
-    output.write("Input File,Station,Average House Price,Destination,Duration,Frequency,Changes,Primary Schools Link,Secondary Schools Link\n")
+    #output.write("Input File,Station,Average House Price,Destination,Duration,Frequency,Changes,Primary Schools Link,Secondary Schools Link\n")
 
     Dir.foreach(@input_dir) do |file_name|
       next if file_name == '.' || file_name == '..'
@@ -23,29 +22,15 @@ class CSVFileAggregatorAndLoader
         secondary_url = "http://home.rm.com/schoolfinder/ShowSchools.aspx?l=#{escaped_name}&t=sec"
         area_details = "#{file_name},#{value.name},#{value.average_house_price}"
 
-        value.journeys.each{|journey| output.write "#{area_details},#{journey.destination_station},#{journey.duration},#{journey.frequency},#{journey.changes},#{primary_url},#{secondary_url}\n" }
+        #value.journeys.each{|journey| output.write "#{area_details},#{journey.destination_station},#{journey.duration},#{journey.frequency},#{journey.changes},#{primary_url},#{secondary_url}\n" }
+
         value.save
       end
 
       puts "Done. #{areas_map.size}"
     end
-    output.close
+    #output.close
   end
 end
 
 
-ActiveRecord::Base.establish_connection(
-    :adapter  => "mysql",
-    :host     => "localhost",
-    :username => "root",
-    :password => "",
-    :database => "area_finder"
-)
-
-
-
-input_dir = ARGV[0]
-
-aggregator = CSVFileAggregatorAndLoader.new(input_dir)
-
-aggregator.aggregate
