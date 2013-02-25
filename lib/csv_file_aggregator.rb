da2a517738aabd1c9bd4d546661e2962911a5205
jbowkett <james_bowkett@lyagushka.co.uk>
2013-02-25 22:37:36 +0000

    now saves the area object

--- lib/csv_file_aggregator.rb
+++ lib/csv_file_aggregator.rb
@@ -15,8 +15,8 @@ class CSVFileAggregator
     Dir.foreach(@input_dir) do |file_name|
       next if file_name == '.' || file_name == '..'
       puts "Inspecting :[#{file_name}]..."
-      areas = InputCsvParser.new.parse(File.join(@input_dir, file_name))
-      areas.each do |key, value|
+      areas_map = InputCsvParser.new.parse(File.join(@input_dir, file_name))
+      areas_map.each do |key, value|
         escaped_name = key.gsub(/ /, "%20")
 
         primary_url = "http://home.rm.com/schoolfinder/ShowSchools.aspx?l=#{escaped_name}&t=pri"
@@ -24,13 +24,25 @@ class CSVFileAggregator
         area_details = "#{file_name},#{value.name},#{value.average_house_price}"
 
         value.journeys.each{|journey| output.write "#{area_details},#{journey.destination_station},#{journey.duration},#{journey.frequency},#{journey.changes},#{primary_url},#{secondary_url}\n" }
+        value.save
       end
 
-      puts "Done. #{areas.size}"
+      puts "Done. #{areas_map.size}"
     end
     output.close
   end
 end
+
+
+ActiveRecord::Base.establish_connection(
+    :adapter  => "mysql",
+    :host     => "localhost",
+    :username => "root",
+    :password => "",
+    :database => "area_finder"
+)
+
+
 
 input_dir = ARGV[0]
 
