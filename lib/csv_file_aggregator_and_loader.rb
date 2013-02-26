186921bdac5c60e3202bd6579e727fa893c8067e
jbowkett <james_bowkett@lyagushka.co.uk>
2013-02-26 23:40:59 +0000

    removed file output

--- lib/csv_file_aggregator_and_loader.rb
+++ lib/csv_file_aggregator_and_loader.rb
@@ -7,29 +7,15 @@ class CSVFileAggregatorAndLoader
   end
 
   def aggregate
-    #output = File.new("commute-from-aggregated.csv", "w")
-
-    #output.write("Input File,Station,Average House Price,Destination,Duration,Frequency,Changes,Primary Schools Link,Secondary Schools Link\n")
-
     Dir.foreach(@input_dir) do |file_name|
       next if file_name == '.' || file_name == '..'
       puts "Inspecting :[#{file_name}]..."
       areas_map = InputCsvParser.new.parse(File.join(@input_dir, file_name))
       areas_map.each do |key, value|
-        escaped_name = key.gsub(/ /, "%20")
-
-        primary_url = "http://home.rm.com/schoolfinder/ShowSchools.aspx?l=#{escaped_name}&t=pri"
-        secondary_url = "http://home.rm.com/schoolfinder/ShowSchools.aspx?l=#{escaped_name}&t=sec"
-        area_details = "#{file_name},#{value.name},#{value.average_house_price}"
-
-        #value.journeys.each{|journey| output.write "#{area_details},#{journey.destination_station},#{journey.duration},#{journey.frequency},#{journey.changes},#{primary_url},#{secondary_url}\n" }
-
         value.save
       end
-
       puts "Done. #{areas_map.size}"
     end
-    #output.close
   end
 end
 
