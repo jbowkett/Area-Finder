5bfaa559c11d1aa8f8e1205020da42bc616fbc68
jbowkett <james_bowkett@lyagushka.co.uk>
2013-02-26 23:41:36 +0000

    extract data

--- lib/school_downloader.rb
+++ lib/school_downloader.rb
@@ -36,7 +36,7 @@ class SchoolDownloader
       school_urls << a_tag['href']
     end
 
-    school_urls.each do | school_url |
+    school_urls.each do |school_url|
       inspect_school(area_to_decorate, school_url, is_primary_results)
     end
   end
@@ -53,20 +53,64 @@ class SchoolDownloader
     gender = get_node_value(@session.find_by_id('content_lvSchoolDetails_lblSchoolGender_0'))
     start_leave_age = get_node_value(@session.find_by_id('content_lvSchoolDetails_lblSchoolAgeRange_0'))
 
-    area_to_decorate.add_school(name, address, gender, start_leave_age, school_type, is_primary_school,
-                                -1, -1, -1, -1, -1, Date.today, distance, school_url, '')
+    overall_inspection_score = extract_score('content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_0')
+    achievement_score = extract_score('content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_1')
+    behaviour_score = extract_score('content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_2')
+    teaching_score = extract_score('content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_3')
+    leadership_score = extract_score('content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_4')
 
-  # content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_0
-  # content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_1
-  # content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_2
-  # content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_3
-  # content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_4
-  # content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_5
-  # content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_6
+    inspection_date = extract_inspection_date
+    ofsted_link = extract_ofsted_link
+
+    area_to_decorate.add_school(name, address, gender, start_leave_age, school_type, is_primary_school,
+                                overall_inspection_score, achievement_score, behaviour_score,
+                                teaching_score, leadership_score, inspection_date, distance,
+                                school_url, ofsted_link)
+  end
+
+  def extract_score(id_name)
+    parent_node = do_xpath_find("//span[@id=\"#{id_name}\"]/../..")
+    result = do_css_find(parent_node, '.chart') if !parent_node.nil?
+    return result['data-value'] if result
+    '-1' # if nothing found
+  end
+
+  def do_css_find(parent_node, css_selector)
+    result = nil
+    begin
+      result = parent_node.find(css_selector)
+    rescue => e
+      puts "cannot find using css :[#{e.message}]"
+    end
+    result
+  end
+
+  def do_xpath_find(x_path)
+    to_return = nil
+    begin
+      to_return = @session.find(:xpath, x_path)
+    rescue => e
+      puts "cannot find using xpath :[#{e.message}]"
+    end
+    to_return
+  end
+
+  def extract_inspection_date
+    parent_node = do_xpath_find("//span[@id=\"content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_5\"]/../..")
+    date_node = do_css_find(parent_node, '.data p') if !parent_node.nil?
+    date_str = date_node.text if !date_node.nil?
+    !date_str.nil? && date_str != '' ? Date.parse(date_str) : Date.today
+  end
+
+  def extract_ofsted_link
+    parent_node = do_xpath_find("//span[@id=\"content_lvQuestions_lvPerformanceIndicators_0_lblValueLabel_6\"]/../..")
+    result = do_css_find(parent_node, '.data a') if !parent_node.nil?
+    return result['href'] if result
+    nil # if nothing found
   end
 
   def get_node_value(node)
-    if(node.nil?)
+    if (node.nil?)
       ''
     else
       node.text
@@ -76,6 +120,7 @@ class SchoolDownloader
 
 end
 
-# tester = SchoolDownloader.new
-# tester.inspect_area('broxbourne')
+
+#tester = SchoolDownloader.new
+#tester.inspect_school(nil, 'http://home.rm.com/SchoolFinder/Search-Result/2032922/London/Alexander-McLeod-Primary-School.aspx', true)
 # tester.inspect_area('berkhamsted')
