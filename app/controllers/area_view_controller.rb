1d8b9bd8d5bdfbf6fd4e39f3997386d23a6d60ab
jbowkett <james_bowkett@lyagushka.co.uk>
2013-04-04 21:33:28 +0100

    more details from summary

--- app/controllers/area_view_controller.rb
+++ app/controllers/area_view_controller.rb
@@ -1,5 +1,9 @@
 class AreaViewController < ApplicationController
   def index
-    puts "hello dave " * 80
+    id = params[:id]
+    @summary = AreaSummary.find(id)
+    @area = @summary.area
+    @primary_schools = @area.schools.select(&:is_primary?)
+    @secondary_schools = @area.schools.reject(&:is_primary?)
   end
 end
