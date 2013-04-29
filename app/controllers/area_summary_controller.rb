10a1c393e66b79aff6ceca9e95ef214349ac1270
jbowkett <james_bowkett@lyagushka.co.uk>
2013-04-29 12:16:13 +0100

    Corrected controller

--- app/controllers/area_summary_controller.rb
+++ app/controllers/area_summary_controller.rb
@@ -1,8 +1,8 @@
 class AreaSummaryController < ApplicationController
   def index
-    all_areas = AreaSummary.find(:all, :conditions => ["status != 'duplicate' "], :order => 'name')
+    all_areas = AreaSummary.all.sort {|a,b| a.name <=> b.name }
 
-    @areas_with_schools = all_areas.select{|area|area.schools.size >0 }
+    @areas_with_schools = all_areas.select{|summary|summary.area.schools.size >0 }
     @areas_yet_to_be_downloaded = all_areas - @areas_with_schools
   end
 end
