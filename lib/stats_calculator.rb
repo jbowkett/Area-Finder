f3f7d6c5f76b4bf8a7c41559cc38320833a6d490
jbowkett <james_bowkett@lyagushka.co.uk>
2013-08-23 17:53:33 +0100

    median of 4 numbers now passing

--- lib/stats_calculator.rb
+++ lib/stats_calculator.rb
@@ -3,15 +3,23 @@ class StatsCalculator
 
   def self.calculate_median(scores)
     sorted_scores = scores.sort()
+    half_length = scores.length / 2
     if length_is_even(scores)
-
+      base_half_length = half_length.to_i
+      middle_two_values = [scores[base_half_length], scores[base_half_length +1]]
+      return calculate_mean(middle_two_values)
     else
-      return sorted_scores[scores.length / 2]
+      return sorted_scores[half_length]
     end
   end
 
   def self.length_is_even(scores)
     scores.length % 2 == 0
+  end
+
+  def self.calculate_mean(scores)
+    total = scores.inject(0){|score, tally| tally+score}
+    return total/scores.length
   end
 
 end
--- spec/stats_calculator_spec.rb
+++ spec/stats_calculator_spec.rb
@@ -10,4 +10,12 @@ describe StatsCalculator do
       StatsCalculator.calculate_median(values).should == 5
     end
   end
+
+  context 'with 4 values' do
+    let(:values) { [5,1,5,1]}
+
+    it 'should calculate median' do
+      StatsCalculator.calculate_median(values).should == 3
+    end
+  end
 end
