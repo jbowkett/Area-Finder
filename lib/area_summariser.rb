cec7effe2f50ae8d617967196231952042f06e2d
jbowkett <james_bowkett@lyagushka.co.uk>
2013-08-23 17:52:30 +0100

    median of 4 numbers now passing

--- lib/area_summariser.rb
+++ lib/area_summariser.rb
@@ -8,4 +8,9 @@ class AreaSummariser
   def self.shortest_journey(area)
     area.journeys.min_by{|journey|journey.duration}
   end
+
+  def self.median_overall_inspection_grade_for_school_effectiveness_score_for(schools)
+    scores = schools.collect(&:overall_inspection_grade_for_school_effectiveness_score)
+    StatsCalculator.calculate_median(scores)
+  end
 end
