7cfd2785a22834461a013f785246d6ddbf202788
jbowkett <james_bowkett@lyagushka.co.uk>
2013-08-23 18:03:48 +0100

    refactoring

--- lib/stats_calculator.rb
+++ lib/stats_calculator.rb
@@ -1,25 +1,24 @@
 
 class StatsCalculator
 
-  def self.calculate_median(scores)
-    sorted_scores = scores.sort()
-    half_length = scores.length / 2
-    if length_is_even(scores)
-      base_half_length = half_length.to_i
-      middle_two_values = [scores[base_half_length], scores[base_half_length +1]]
+  def self.calculate_median(values)
+    sorted_scores = values.sort()
+    half_length = values.length / 2
+    if length_is_even(values)
+      middle_two_values = [values[half_length], values[half_length +1]]
       return calculate_mean(middle_two_values)
     else
       return sorted_scores[half_length]
     end
   end
 
-  def self.length_is_even(scores)
-    scores.length % 2 == 0
+  def self.length_is_even(values)
+    values.length % 2 == 0
   end
 
-  def self.calculate_mean(scores)
-    total = scores.inject(0){|score, tally| tally+score}
-    return total.to_f/scores.length.to_f
+  def self.calculate_mean(values)
+    total = values.inject(0){|score, tally| tally + score}
+    return total.to_f/values.length.to_f
   end
 
 end
--- spec/stats_calculator_spec.rb
+++ spec/stats_calculator_spec.rb
@@ -26,4 +26,29 @@ describe StatsCalculator do
       StatsCalculator.calculate_mean(values).should == 3
     end
   end
+
+  context 'with 5 unique values' do
+    let(:values) { [9,4,7,5,3]}
+
+    it 'should calculate median' do
+      StatsCalculator.calculate_median(values).should == 5
+    end
+
+    it 'should calculate mean' do
+      StatsCalculator.calculate_mean(values).should == 5.6
+    end
+  end
+
+  context 'with 6 unique values' do
+    let(:values) { [26,3,52,7,31]}
+
+    it 'should calculate median' do
+      StatsCalculator.calculate_median(values).should == 26
+    end
+
+    it 'should calculate mean' do
+      StatsCalculator.calculate_mean(values).should == 23.8
+    end
+  end
+
 end
